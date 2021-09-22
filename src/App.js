b153f107e370198b178096673f11782a5e350e35
Jason Viox <jrv04@hotmail.com>
2021-09-22 09:16:24 -0500

    Start framework

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import About from './assets/components/About.js';
+import Header from './assets/components/Header.js';
+import MainProject from './assets/components/MainProject.js';
+import projects from './assets/components/ProjectData.json'
+
 function App() {
-  return <main />;
+  return( <div>
+  <Header/>
+  <About/>
+  {projects.map((project, index) => ( <MainProject key={index} project={project}/>))}
+  </div>);
 }
 
 export default App;
