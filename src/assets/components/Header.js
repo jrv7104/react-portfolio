0e7083bf585120067a1120f1ca7030dcde6c6129
Jason Viox <jrv04@hotmail.com>
2021-09-22 09:17:51 -0500

    Build header

---
+++ src/assets/components/Header.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Image from "../images/profile_image.jpg";
+
+function Header() {
+    return(
+        <header>
+        <img src={Image} className="profile-photo" alt="Photo coming soon!" width="20%" height="20%"/>
+        <h1>Jason Viox</h1>
+        <nav className="links">
+            <a href="#about">About Me</a>
+            <a href="#work">Work</a>
+            <a href="#contact">Contact Me</a>
+
+            <a href="./Jason Viox_Resume.pdf" target="_blank" rel="noopener noreferrer">Resume</a>
+        </nav>
+    </header>
+    )
+}
+
+export default Header;
