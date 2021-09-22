c51f0cf12898d5d57dd86ff6202fc527ec5c31e7
Jason Viox <jrv04@hotmail.com>
2021-09-22 09:17:39 -0500

    Improved About section

--- src/assets/components/About.js
+++ src/assets/components/About.js
@@ -2,29 +2,17 @@ import React from 'react';
 
 function About() {
     return (
-        <section>
-
-            <p>
-            Hello, and welcome to my portfolio page! My name is Jason Viox (pronounced Vee-ox), 
-            and I have been interested in coding and web development for about a year. I became 
-            interested in the field around the time the COVID-19 pandemic hit, and my journalism 
-            career was affected by the incident. I left the field after spending a total of 10 
-            years in journalism, but that has now allowed me to invest more time in coding.
-            </p>
-
-            <p>
-            In regards to my coding experience, I started with the CS50 online class through
-            Harvard University, but later explored Mimo, a phone app that teaches HTML, JavaScript,
-            CSS, SQL and Python. While those lessons have been beneficial, I am currently taking a 
-            web development bootcamp through Washington University with the goals of obtaining a full 
-            stack certification and starting a new career.
-            </p>
-
-            <p>
-            Thank you for stopping by. Feel free to contact me for any questions or even just to say hi!
-            </p>
-        </section>
-    );
+        <section className="page-section" id="about">
+    <div>
+        <h2 className="greeting">Welcome to my portfolio page!</h2>
+    </div>
+    <div>
+        <p>Hello, and welcome to my portfolio page! My name is Jason Viox (pronounced Vee-ox), and I have been interested in coding and web development for about a year. I became interested in the field around the time the COVID-19 pandemic hit, and my journalism career was affected by the incident. I left the field after spending a total of 10 years in journalism, but that has now allowed me to invest more time in coding.</p>
+        <p>In regards to my coding experience, I started with the CS50 online class through Harvard University, but later explored Mimo, a phone app that teaches HTML, JavaScript, CSS, SQL and Python. While those lessons have been beneficial, I am currently taking a web development bootcamp through Washington University with the goals of obtaining a full stack certification and starting a new career.</p>
+        <p>Thank you for stopping by. Feel free to contact me for any questions or even just to say hi!</p>
+    </div>
+</section>
+    )
 }
 
 export default About;
