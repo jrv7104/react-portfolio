fe1ad5be2d81de1a4006d10a1fed0c8f2d5188e1
Jason Viox <jrv04@hotmail.com>
2021-09-25 09:01:52 -0500

    npm run build

--- src/assets/components/Portfolio.js
+++ src/assets/components/Portfolio.js
@@ -1,7 +1,24 @@
 import React from "react";
 
 function Portfolio(props) {
-
+    return (
+        <div className="project pwdGen">
+        <a
+            href={props.project.deploy}
+            target="_blank"
+            rel='noopener noreferrer'>
+            <h3>Password Generator</h3>
+        </a>
+        <span>
+        <a
+                    href={props.project.GitHub}
+                    target='_blank'
+                    rel='noopener noreferrer'>
+                    <h3>GitHub link</h3>
+                </a>{' '}
+            </span>
+        </div>
+    );
 }
 
 export default Portfolio;
