8a29f18fdf3f03db870b89cc6a1d898d3b5dc887
Jason Viox <jrv04@hotmail.com>
2021-09-25 08:23:57 -0500

    Build Contact section

--- src/assets/components/Contact.js
+++ src/assets/components/Contact.js
@@ -1,7 +1,19 @@
 import React from 'react';
 
 function Contact() {
-
+    return (
+        <section className="page-section contact" id="contact">
+    <h2>Contact Me</h2>
+    <div>
+        <address>
+            <a href="tel:+13148524554">314-852-4554</a>
+            <a href="mailto:jrv04@hotmail.com">jrv04@hotmail.com</a>
+            <a href="https://github.com/jrv7104" target="_blank" rel="noopener noreferrer">GitHub</a>
+            <a href="https://www.linkedin.com/in/jason-viox-8477b7131" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+        </address>
+    </div>
+    </section>
+    );
 }
 
 export default Contact;
