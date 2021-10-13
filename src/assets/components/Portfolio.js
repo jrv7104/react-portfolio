ff2a98defc4be63dc9d0fb6faea2f8cbc4ae76fe
Jason Viox <jrv04@hotmail.com>
2021-10-13 10:10:42 -0500

    Correct portfolio setup

--- src/assets/components/Portfolio.js
+++ src/assets/components/Portfolio.js
@@ -1,24 +1,16 @@
 import React from "react";
+import MainProject from './MainProject';
+import projects from './ProjectData.json'
 
-function Portfolio(props) {
-    console.log(props.project.class)
+function Portfolio() {
     return (
-        <div className={props.project.class}>
-        <a
-            href={props.project.deploy}
-            target="_blank"
-            rel='noopener noreferrer'>
-            <h3>Password Generator</h3>
-        </a>
-        <span>
-        <a
-                    href={props.project.GitHub}
-                    target='_blank'
-                    rel='noopener noreferrer'>
-                    <h3>GitHub link</h3>
-                </a>{' '}
-            </span>
-        </div>
+        <section className="page-section">
+        <div>
+                      {projects.map((project, index) => (
+                      <MainProject key={index} project={project} />
+                      ))}
+                      </div>
+                  </section>
     );
 }
 
