440b3f1ca9c4d03ca948ff7bfb4208052246d92b
Jason Viox <jrv04@hotmail.com>
2021-10-13 09:29:09 -0500

    Import GitHub and LinkedIn logos

--- src/assets/components/Contact.js
+++ src/assets/components/Contact.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import GitHub from '../logos/GitHub logo.png';
+import LinkedIn from '../logos/LinkedIn logo.png';
 
 function Contact() {
     return (
@@ -8,8 +10,8 @@ function Contact() {
         <address>
             <a href="tel:+13148524554">314-852-4554</a>
             <a href="mailto:jrv04@hotmail.com">jrv04@hotmail.com</a>
-            <a href="https://github.com/jrv7104" target="_blank" rel="noopener noreferrer">GitHub</a>
-            <a href="https://www.linkedin.com/in/jason-viox-8477b7131" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+            <a href="https://github.com/jrv7104" target="_blank" rel="noopener noreferrer"><img src={GitHub} className="GitHub logo"/></a>
+            <a href="https://www.linkedin.com/in/jason-viox-8477b7131" target="_blank" rel="noopener noreferrer"><img src={LinkedIn} className="LinkedIn logo"/></a>
         </address>
     </div>
     </section>
