59767adb0901fea8c4ae12ad1af3e8c7b854e335
Jason Viox <jrv04@hotmail.com>
2021-10-15 11:30:28 -0500

    Removed contact info to footer

--- src/assets/components/Contact.js
+++ src/assets/components/Contact.js
@@ -3,19 +3,9 @@ import GitHub from '../logos/GitHub logo.png';
 import LinkedIn from '../logos/LinkedIn logo.png';
 
 function Contact() {
-    return (
-        <section className="page-section contact" id="contact">
-    <h2>Contact Me</h2>
-    <div>
-        <address>
-            <a href="tel:+13148524554">314-852-4554</a>
-            <a href="mailto:jrv04@hotmail.com">jrv04@hotmail.com</a>
-            <a href="https://github.com/jrv7104" target="_blank" rel="noopener noreferrer"><img src={GitHub} className="GitHub logo"/></a>
-            <a href="https://www.linkedin.com/in/jason-viox-8477b7131" target="_blank" rel="noopener noreferrer"><img src={LinkedIn} className="LinkedIn logo"/></a>
-        </address>
-    </div>
-    </section>
-    );
+    return(  
+        <h1>Contact Me</h1>
+    )
 }
 
 export default Contact;
