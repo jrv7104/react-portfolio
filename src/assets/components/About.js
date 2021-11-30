bcea3692c3cd589fa8ff65888833d99deb92c6f4
Jason Viox <jrv04@hotmail.com>
2021-11-30 17:57:28 -0600

    Reedit about.js section

--- src/assets/components/About.js
+++ src/assets/components/About.js
@@ -8,7 +8,7 @@ function About() {
     </div>
     <div>
         <p>Hello, and welcome to my portfolio page! My name is Jason Viox (pronounced Vee-ox), and I have been interested in coding and web development for about a year. I became interested in the field around the time the COVID-19 pandemic hit, and my journalism career was affected by the incident. I left the field after spending a total of 10 years in journalism, but that has now allowed me to invest more time in coding.</p>
-        <p>In regards to my coding experience, I started with the CS50 online class through Harvard University, but later explored Mimo, a phone app that teaches HTML, JavaScript, CSS, SQL and Python. While those lessons have been beneficial, I recently  graduated from a coding and web development bootcamp through Washington University. I accomplished one of my biggest goals by graduating from the program and earning my certificate of completion. Now I am working towards my next goal of starting a new career in coding and web development.</p>
+        <p>In regards to my coding experience, I started with the CS50 online class through Harvard University, but later explored Mimo, a phone app that teaches HTML, JavaScript, CSS, SQL and Python. I recently  graduated from a coding and web development bootcamp through Washington University and received a certificate of completion. Now I am working towards my next goal of starting a new career in coding and web development.</p>
         <p>Thank you for stopping by. Feel free to contact me for any questions or even just to say hi!</p>
     </div>
 </section>
